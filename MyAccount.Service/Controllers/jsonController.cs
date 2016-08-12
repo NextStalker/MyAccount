6f515710873c7d633b5788e8a7f8ddefb3ee43b9
Pavel Maslyaev <pavel-max@rambler.ru>
2016-08-12 16:07:02 +0300

    добавил контроллер

---
+++ MyAccount.Service/Controllers/jsonController.cs
@@ -0,0 +1,19 @@
+﻿using MyAccount.Service.FirstService;
+using MyAccount.Service.Logging;
+using Serilog;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyAccount.Service.Controllers
+{
+    public class jsonController : ApiController
+    {
+        private static ILogger logger = LogManager.GetLogger();
+
+        public void Post(SyncProfileRequest json)
+        {
+            SyncProfileRequestList.Instance.Add(json);
+        }
+    }
+}
